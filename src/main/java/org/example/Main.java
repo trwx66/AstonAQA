0bfbb3346a30fbfcb11adc9218f35fb69582954c
trwx66 <trwx66@gmail.com>
2024-06-27 21:52:56 +0300

    Lesson_9

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,39 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
- * Hello world!
  *
  */
-public class Main
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!!" );
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Задание №1\n");
+
+        String string = "на дворе трава на траве дрова не руби дрова на траве двора";
+        String[] array = string.split(" ");
+        System.out.println("Начальный массив " + Arrays.toString(array) + "\n");
+
+        Map<String, Integer> uniqueWordsCounter = new HashMap<>();
+        for (String word : array) {
+            if (uniqueWordsCounter.containsKey(word)) {
+                uniqueWordsCounter.put(word, uniqueWordsCounter.get(word) + 1);
+            } else uniqueWordsCounter.put(word, 1);
+        }
+
+        Set<String> uniqueWords = uniqueWordsCounter.keySet();
+        System.out.println("Список уникальных слов " + uniqueWords + "\n");
+
+        for (Map.Entry<String, Integer> entry : uniqueWordsCounter.entrySet()) {
+            System.out.printf("Слово \"%s\" повторяется %d раз \n", entry.getKey(), entry.getValue());
+        }
+
+        System.out.println("Задание №2\n");
+
     }
 }
 
