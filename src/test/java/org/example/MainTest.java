138b9618d7d86dab0bc2ee262937820b40e0db69
trwx66 <trwx66@gmail.com>
2024-07-03 09:20:53 +0300

    Lesson_9

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    public void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
