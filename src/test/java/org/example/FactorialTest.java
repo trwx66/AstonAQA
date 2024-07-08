40d786c74494b3a8b9af7cd0257afeec3c28a069
trwx66 <trwx66@gmail.com>
2024-07-08 10:24:10 +0300

    Lesson_10_testng

--- src/test/java/org/example/FactorialTest.java
+++ src/test/java/org/example/FactorialTest.java
@@ -1,9 +1,8 @@
 package org.example;
 
-import org.testng.Assert;
 import org.testng.annotations.*;
-
 import java.math.BigInteger;
+import static org.testng.Assert.*;
 
 public class FactorialTest {
     private Factorial factorial;
@@ -28,31 +27,23 @@ public class FactorialTest {
         factorial = null;
     }
 
-    @Test(priority = 1)
-    public void testFactorialOfZero() {
-        System.out.printf("Запускаю testFactorialOfZero%nФакториал 0 = %d%n%n", factorial.getFactorial(0));
-        Assert.assertEquals(factorial.getFactorial(0), BigInteger.ONE, "Факториал 0");
+    @DataProvider(name = "factorialData")
+    public Object[][] createFactorialData() {
+        return new Object[][] {
+                {0, BigInteger.ONE},
+                {1, BigInteger.ONE},
+                {5, BigInteger.valueOf(120)},
+                {20, new BigInteger("2432902008176640000")}
+        };
     }
 
-    @Test(priority = 2)
-    public void testFactorialOfOne() {
-        System.out.printf("Запускаю testFactorialOfOne%nФакториал 1 = %d%n%n", factorial.getFactorial(1));
-        Assert.assertEquals(factorial.getFactorial(1), BigInteger.ONE, "Факториал 1");
+    @Test(dataProvider = "factorialData", priority = 1)
+    public void testFactorial(int input, BigInteger expected) {
+        System.out.printf("Запускаю testFactorial%nФакториал %d = %d%n%n", input, factorial.getFactorial(input));
+        assertEquals(factorial.getFactorial(input), expected, "Факториал " + input);
     }
 
-    @Test(priority = 3)
-    public void testFactorialOfPositiveNumber() {
-        System.out.printf("Запускаю testFactorialOfPositiveNumber%nФакториал 5 = %d%n%n", factorial.getFactorial(5));
-        Assert.assertEquals(factorial.getFactorial(5), BigInteger.valueOf(120), "Факториал 5");
-    }
-
-    @Test(priority = 4)
-    public void testFactorialOfLargeNumber() {
-        System.out.printf("Запускаю testFactorialOfLargeNumber%nФакториал 20 = %d%n%n", factorial.getFactorial(20));
-        Assert.assertEquals(factorial.getFactorial(20), new BigInteger("2432902008176640000"), "Факториал 20");
-    }
-
-    @Test(expectedExceptions = IllegalArgumentException.class, priority = 5)
+    @Test(expectedExceptions = IllegalArgumentException.class, priority = 2)
     public void testFactorialOfNegativeNumber() {
         System.out.println("Запускаю testFactorialOfNegativeNumber");
         factorial.getFactorial(-1);
