ddaf8b2266ac601daf1a856e01f2a0b4fc53ca22
trwx66 <trwx66@gmail.com>
2024-07-15 23:35:30 +0300

    Lesson_13

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,8 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class Main
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!!" );
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
     }
 }
 
