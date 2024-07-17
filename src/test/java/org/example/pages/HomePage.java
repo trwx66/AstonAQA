1cbbbfbf87fb386f2cff9a1a2f81dbc929af7b85
trwx66 <trwx66@gmail.com>
2024-07-17 16:09:11 +0300

    Lesson_13

--- src/test/java/org/example/pages/HomePage.java
+++ src/test/java/org/example/pages/HomePage.java
@@ -77,8 +77,6 @@ public class HomePage {
     }
 
     public boolean fillFormAndSubmit(String phone, String amount, String email) {
-
-        wait.until(ExpectedConditions.visibilityOf(phoneInput));
         servicesDropdown.click();
         communicationServices.click();
         phoneInput.sendKeys(phone);
